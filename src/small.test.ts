c798177b6d6edfd21da3ed913ae53113674a2d58
Elena Cavallero <elenacavallero@outlook.com>
2022-04-16 12:52:20 +0100

    add small

---
+++ src/small.test.ts
@@ -0,0 +1,32 @@
+import getPerimeter from "./small";
+
+describe("Desert Island", () => {
+  it("Calculates the correct perimeter of a map with one row", () => {
+    expect(getPerimeter([[1, 0, 1, 0]])).toBe(3);
+  });
+
+  it("Calculates the correct perimeter of a map with one column", () => {
+    expect(getPerimeter([[1], [1], [0], [1]])).toBe(2);
+  });
+
+  it("Calculates the correct perimeter of a map", () => {
+    expect(
+      getPerimeter([
+        [0, 0],
+        [0, 1],
+      ])
+    ).toBe(2);
+  });
+
+  it("Calculates the correct perimeter of a map", () => {
+    expect(
+      getPerimeter([
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+      ])
+    ).toBe(12);
+  });
+});
